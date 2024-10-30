f816e8a37592ca27588948ed5f5d7574227317a0
Hanna K <anna_kon@bigmir.net>
2024-10-30 23:40:03 +0200

    added band page tests

---
+++ base-concert-venue/__tests__/ui/band.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import BandComponent from "@/pages/bands/[bandId]";
+import { readFakeData } from '@/__tests__/__mocks__/fakeData';
+
+test("band component displays correct band information", async () => {
+  // this func needs to be async, because we are reading files
+  const { fakeBands } = await readFakeData();
+  render(<BandComponent band={fakeBands[0]} error={null} />);
+  const heading = screen.getByRole("heading", { name: /the wandering bunnies/i });
+  expect(heading).toBeInTheDocument();
+  // in real life we can add tests:
+  // the image, the description, the link to the photograper
+
+});
+
+test("band component displays an error if an error occurs", () => {
+  render(<BandComponent band={null} error="Something went wrong!" />);
+  const error = screen.getByRole("heading", { name: /Something went wrong/i });
+  expect(error).toBeInTheDocument();
+} )
